35f8e9d8483105ed390f0ed32f85e694621acd8e
Maddin-619 <Martin.Brenner94@gmail.com>
2023-11-23 10:27:07 +0100

    update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -39,30 +39,8 @@ require("lazy").setup({
       "stevearc/vim-vsnip-snippets",
     }
   },
-  {
-    "tami5/lspsaga.nvim",
-    config = function()
-      local map = vim.api.nvim_buf_set_keymap
-      --[[ map(0, "n", "gr", "<cmd>Lspsaga rename<cr>", {silent = true, noremap = true}) ]]
-      --[[ map(0, "n", "gx", "<cmd>Lspsaga code_action<cr>", {silent = true, noremap = true}) ]]
-      --[[ map(0, "x", "gx", ":<c-u>Lspsaga range_code_action<cr>", {silent = true, noremap = true}) ]]
-      --[[ map(0, "n", "K",  "<cmd>Lspsaga hover_doc<cr>", {silent = true, noremap = true}) ]]
-      --[[ map(0, "n", "go", "<cmd>Lspsaga show_line_diagnostics<cr>", {silent = true, noremap = true}) ]]
-      --[[ map(0, "n", "gj", "<cmd>Lspsaga diagnostic_jump_next<cr>", {silent = true, noremap = true}) ]]
-      --[[ map(0, "n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", {silent = true, noremap = true}) ]]
-      --[[ map(0, "n", "<C-u>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1, '<c-u>')<cr>", {}) ]]
-      --[[ map(0, "n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<cr>", {}) ]]
-      map(0, "n", "<leader>ca", "<cmd>lua require('lspsaga.codeaction').code_action()<cr>",
-        { silent = true, noremap = true })
-      --[[ nnoremap <silent><leader>ca <cmd>lua require('lspsaga.codeaction').code_action()<CR> ]]
-      --[[ vnoremap <silent><leader>ca :<C-U>lua require('lspsaga.codeaction').range_code_action()<CR> ]]
-    end
-
-  },
-
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
-
   {
     "jose-elias-alvarez/null-ls.nvim",
     dependencies = { "nvim-lua/plenary.nvim" }
@@ -270,6 +248,7 @@ require("lazy").setup({
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
+    opts = {}
   },
   {
     "windwp/nvim-ts-autotag",
@@ -314,11 +293,11 @@ require("lazy").setup({
     config = function()
       require('telescope').load_extension('fzf')
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>j', '<cmd>Telescope find_files hidden=true<cr>', {})
       vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>r', builtin.grep_string, {})
       vim.keymap.set('n', '<M-o>', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>j', builtin.oldfiles, {})
+      vim.keymap.set('n', '<leader>f', builtin.oldfiles, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     end
   },
